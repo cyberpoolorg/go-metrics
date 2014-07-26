58b61b1c015e9fd232192208aa52b1ba0f64203a
Yuriy Vasiyarov <yuriy.vasiyarov@lazada.com>
2014-07-26 19:51:00 +0700

    Add prefix support

--- librato/librato.go
+++ librato/librato.go
@@ -2,7 +2,7 @@ package librato
 
 import (
 	"fmt"
-	"github.com/rcrowley/go-metrics"
+	"github.com/yvasiyarov/go-metrics"
 	"log"
 	"math"
 	"regexp"
@@ -27,10 +27,19 @@ type Reporter struct {
 	Registry        metrics.Registry
 	Percentiles     []float64              // percentiles to report on histogram metrics
 	TimerAttributes map[string]interface{} // units in which timers will be displayed
+	MetricPrefix    string
 }
 
 func NewReporter(r metrics.Registry, d time.Duration, e string, t string, s string, p []float64, u time.Duration) *Reporter {
-	return &Reporter{e, t, s, d, r, p, translateTimerAttributes(u)}
+	return &Reporter{
+		Email:           e,
+		Token:           t,
+		Source:          s,
+		Interval:        d,
+		Registry:        r,
+		Percentiles:     p,
+		TimerAttributes: translateTimerAttributes(u),
+	}
 }
 
 func Librato(r metrics.Registry, d time.Duration, e string, t string, s string, p []float64, u time.Duration) {
@@ -41,11 +50,13 @@ func (self *Reporter) Run() {
 	ticker := time.Tick(self.Interval)
 	metricsApi := &LibratoClient{self.Email, self.Token}
 	for now := range ticker {
+
 		var metrics Batch
 		var err error
 		if metrics, err = self.BuildRequest(now, self.Registry); err != nil {
 			log.Printf("ERROR constructing librato request body %s", err)
 		}
+
 		if err := metricsApi.PostMetrics(metrics); err != nil {
 			log.Printf("ERROR sending metrics to librato %s", err)
 		}
@@ -83,8 +94,13 @@ func (self *Reporter) BuildRequest(now time.Time, r metrics.Registry) (snapshot 
 	snapshot.Counters = make([]Measurement, 0)
 	histogramGaugeCount := 1 + len(self.Percentiles)
 	r.Each(func(name string, metric interface{}) {
+
+		if self.MetricPrefix != "" {
+			name = self.MetricPrefix + name
+		}
 		measurement := Measurement{}
 		measurement[Period] = self.Interval.Seconds()
+
 		switch m := metric.(type) {
 		case metrics.Counter:
 			measurement[Name] = fmt.Sprintf("%s.%s", name, "count")
